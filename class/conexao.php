3eff35505889ea1c76e238d887afbe293e17dad1
BrunoCatto <110671540+BrunoCatto@users.noreply.github.com>
2025-12-26 09:33:59 -0300

    Add files via upload

---
+++ class/conexao.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$servidor = "localhost";
+$usuario = "uuccknpg_glmidia";
+$senha = "192031Bc@93";
+$dbname = "uuccknpg_glmidia";
+$link2 = "vipervpn.alphi.media";
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+//.
+
+?>
